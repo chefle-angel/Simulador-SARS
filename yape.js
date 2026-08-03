0d37777f77a4b62d8e5bc2f43ed14a7f0c65fc9e
chefle-angel <angelmaringavilan06@gmail.com>
2026-08-03 16:36:03 +0200

    Initialize Express server with IP logging

---
+++ yape.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Necesario en Render para leer la IP real detrás de su proxy
+app.set('trust proxy', true); 
+
+app.get('/', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log('IP capturada:', ip);
+    res.send('Página cargada');
+});
+
+app.listen(3000, () => console.log('Servidor activo en puerto 3000'));
